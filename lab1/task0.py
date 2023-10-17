ad0f0f6e23635de25125ea09f49a593708f6b535
inwk6312course <inwk6312course@gmail.com>
2023-10-17 19:54:13 -0300

    lab1

--- lab1/task0.py
+++ lab1/task0.py
@@ -1,1 +1,7 @@
-print("hello world")
+import logging
+
+logging.debug('This is a debug message')
+logging.info('This is an info message')
+logging.warning('This is a warning message')
+logging.error('This is an error message')
+logging.critical('This is a critical message')
